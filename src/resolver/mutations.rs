3841821d593a0d2d0038a185857f4c4191de6b27
tanay-pingalkar <tpingalkar@gmail.com>
2021-06-24 17:21:16 +0530

    first commit

--- src/resolver/mutations.rs
+++ src/resolver/mutations.rs
@@ -4,16 +4,6 @@ use diesel::prelude::*;
 use juniper::{FieldResult, GraphQLInputObject};
 
 #[derive(GraphQLInputObject)]
-struct PostCreate {
-    title: String,
-}
-
-#[derive(GraphQLInputObject)]
-struct PostDelete {
-    id: i32,
-}
-
-#[derive(GraphQLInputObject)]
 struct PostUpdate {
     id: i32,
     new_title: String,
@@ -23,22 +13,20 @@ pub struct MutationRoot;
 
 #[juniper::graphql_object(Context = Context)]
 impl MutationRoot {
-    fn post(ctx: &Context, post_info: PostCreate) -> FieldResult<Post> {
+    fn post(ctx: &Context, title: String) -> FieldResult<Post> {
         use crate::schema::posts;
         let conn = &mut ctx.pool.get().unwrap();
         let p = diesel::insert_into(posts::table)
-            .values(NewPost {
-                title: post_info.title,
-            })
+            .values(NewPost { title })
             .get_result::<Post>(conn)
             .expect("error while creating post");
         Ok(p)
     }
 
-    fn delete(ctx: &Context, post_info: PostDelete) -> FieldResult<Post> {
+    fn delete(ctx: &Context, post_id: i32) -> FieldResult<Post> {
         use crate::schema::posts::dsl::*;
         let conn = &mut ctx.pool.get().unwrap();
-        let p = diesel::delete(posts.filter(id.eq(post_info.id)))
+        let p = diesel::delete(posts.filter(id.eq(post_id)))
             .get_result::<Post>(conn)
             .expect("error while deleting post");
         Ok(p)
